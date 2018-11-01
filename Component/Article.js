72b090211d351e35865bf8a19ee14cec348222c0
rezamasrori <39959221+rezamasrori@users.noreply.github.com>
2018-11-01 22:32:40 +0330

    Add files via upload

--- Component/Article.js
+++ Component/Article.js
@@ -1,24 +1,25 @@
-import React , {Component} from 'react';
+import React, { Component } from 'react';
 
-import { Image , StyleSheet , TouchableHighlight} from 'react-native';
+import { Image, StyleSheet, TouchableHighlight, Alert , View } from 'react-native';
 
-import { Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body , Drawer, Container, Right } from 'native-base';
+import { Card, CardItem, Left, Body, Text, Thumbnail, Icon, Button } from 'native-base';
 
+import HTML from 'react-native-render-html';
+// import {parse}from 'fast-html-parser';
 
-export default class Article extends Component{
-
-
-    
-    
+export default class Article extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            Products:[],
+            Products: [],
             imgURL: '',
             page: 1,
             // LinkURL:[],
         }
+        // var root =parse( Products.item.content.rendered);
+        // console.log('here ')
+        // console.log(root)
 
     }
     /*برای فراخوانی از 
@@ -26,80 +27,80 @@ export default class Article extends Component{
     استفاده میکنیم چون تویه 
     constructor 
     ریختیم */
-    componentDidMount(){
+    componentDidMount() {
 
         fetch(this.props.Products.item._links['wp:featuredmedia'][0].href)
-        .then(Response=> Response.json())
-        .then(json => {
-            
-            console.log(json.guid.rendered)
-            this.setState({
-                imgURL: json.guid.rendered
+            .then(Response => Response.json())
+            .then(json => {
+
+                console.log(json.guid.rendered)
+                this.setState({
+                    imgURL: json.guid.rendered
+                });
             });
-            // console.log("HERE");
-            // console.log(this.state.imgURL);
-            
-        });
-
-
-        // console.log(this.props.Products.item._links['wp:featuredmedia'][0].href)
-
     }
-    render()
-    {
-        const {Products} =this.props;
-         return( 
 
-                <TouchableHighlight>
-                    <Card style={{flex: 0}}>
-                        <CardItem>
-                            <Left> 
-                                <Body>
-                                    <Text>{Products.item.title.rendered}</Text>  
-                                        <Text note>{Products.item.alt_text}</Text>
-                                            </Body>
-                                            <Thumbnail source={{uri:this.state.imgURL}} />                       
-                                                </Left>
-                                                    </CardItem>
-                                                        <CardItem>
-                                                            <Body>
-                                                                <Image source={{uri:this.state.imgURL}} 
-                                                                style={{ backgroundColor:'#333', height: 200, width: 200, flex: 1}}
-                                                                />
-                                                                <Text numberOfLines={3}>
-                                                                   {Products.item.content.rendered}
-                                                                </Text>
-   
-                                                            </Body>
-                                                            </CardItem>
-                                                        <CardItem>
-                                                    <Left>
-                                                <Button transparent textStyle={{color: '#87838B'}}>
-                                            <Icon name="logo-github" />
-                                        <Text >{Products.item.date}</Text>
-                                </Button>
-                            </Left>
-                        </CardItem>
-                    </Card> 
+
+    render() {
+        const { Products } = this.props;
+        return (
+            <View>
+                <TouchableHighlight onPress={() => { Alert.alert('O||O') }}>
+                <View>
+                <Card style={{ flex: 0 }}>
+                <View>
+                    <CardItem>
+                        <Left>
+                            <Body>
+                                <View>
+                                <Text>{Products.item.title.rendered}</Text>
+                                <Text note>{Products.item.alt_text}</Text>
+                                </View>
+                            </Body>
+                            <Thumbnail source={{ uri: this.state.imgURL }} />
+                        </Left>
+                    </CardItem>
+                    <CardItem content>
+                <View>
+                    <Body>
+                            <Image source={{ uri: this.state.imgURL }}
+                                style={{ backgroundColor: '#333', height: 200, width: 200, flex: 1 }}
+                            />
+                            <CardItem>
+                                <View>
+                                    <Text>
+                                        <HTML value={Products.item.content.rendered} />
+                                    </Text>
+                                </View>
+                            </CardItem>
+
+
+                        </Body>
+                    </View>
+                       
+                    </CardItem>
+                    <CardItem>
+                        <View>
+                        <Left>
+                            <Button transparent textStyle={{ color: '#87838B' }}>
+                                <Icon name="logo-github" />
+                                
+                                <Text>{Products.item.date}</Text>
+                                
+                            </Button>
+                           </Left>
+                        </View>
+                       
+                    </CardItem>
+                    </View>
+                </Card>
+                </View>
+                
                 </TouchableHighlight>
-            
-         );
-     }
+            </View>
+        );
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
 
 
 
